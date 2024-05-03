0a371f32858df92d46dc362ddc0146fe21062fdf
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-05-03 16:54:49 +0200

    Create Equalize the Array.cs

---
+++ C# Problem Solving Solutions/Equalize the Array.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<int> arr)
+    {
+        Dictionary <int, int> freq = new Dictionary < int, int > ();
+        for (int i = 0; i<arr.Count; i++){
+            if (freq.ContainsKey(arr[i])) {
+                freq[arr[i]] += 1;
+            }
+            else {
+                freq[arr[i]] = 1;
+            }
+        }
+        
+        return arr.Count - freq.Values.Max();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.equalizeArray(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
