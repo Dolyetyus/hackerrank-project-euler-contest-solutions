29a0e61adbb3313bc41fc41a6e6b48e7ed8c54b8
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-05-18 17:31:54 +0200

    Create Taum and B'day.cs

---
+++ C# Problem Solving Solutions/Taum and B'day.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static long taumBday(int b, int w, int bc, int wc, int z)
+    {
+        long bbuy = bc;
+        long wbuy = wc;
+        
+        if (bc+z < wbuy) wbuy = bc+z;
+        if (wc+z < bbuy) bbuy = wc+z;
+        
+        return bbuy*b + wbuy*w;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int b = Convert.ToInt32(firstMultipleInput[0]);
+
+            int w = Convert.ToInt32(firstMultipleInput[1]);
+
+            string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int bc = Convert.ToInt32(secondMultipleInput[0]);
+
+            int wc = Convert.ToInt32(secondMultipleInput[1]);
+
+            int z = Convert.ToInt32(secondMultipleInput[2]);
+
+            long result = Result.taumBday(b, w, bc, wc, z);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
